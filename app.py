26ac3fc48f166c4c43fd384e40b910706ca10f72
strong-sys89 <jtd4v8d8cs@privaterelay.appleid.com>
2026-02-26 00:22:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -235,4 +235,5 @@ def depenses_par_date():
 
 # ------------------ Lancement ------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
